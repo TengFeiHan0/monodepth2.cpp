1b0cbb1eb98f5cd267dc2c95229b4e97673bbdc2
Tengfei han <38068286+TengFeiHan0@users.noreply.github.com>
2020-04-12 22:36:30 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,8 +7,9 @@ using namespace std;
 using namespace cv;
 int main(int argc,char**argv)
 {
+    //load a pytorch model
     torch::jit::script::Module decoder = torch::jit::load("/opt/depth/model.pt");
-    decoder.to(at::kCUDA);
+    decoder.to(at::kCUDA);//put this model on GPU
     cv::Mat src=cv::imread("/opt/depth/test_image.png");
     cv::Mat input_mat;
     int w=640;
@@ -16,19 +17,20 @@ int main(int argc,char**argv)
     
     cv::resize(src,input_mat,cv::Size(w,h));
     input_mat.convertTo(input_mat,CV_32FC3,1./255.);
+    //transform a cv::Mat into a tensor
     torch::Tensor tensor_image = torch::from_blob(input_mat.data, {1,input_mat.rows, input_mat.cols,3}, torch::kF32);
     tensor_image = tensor_image.permute({0,3,1,2});
     tensor_image = tensor_image.to(at::kCUDA);
 
-    std::vector<torch::IValue> batch;
+    std::vector<torch::IValue> batch;//inputs
     batch.push_back(tensor_image);
     //batch.clear();
     
-    auto outputs= decoder.forward(batch);
-    auto outputs_tu = outputs.toTuple();
-    auto tensor_output = outputs_tu->elements()[3].toTensor().to(at::kCPU);
+    auto outputs= decoder.forward(batch);// inference
+    auto outputs_tu = outputs.toTuple();//this model return four disp images with different sizes.
+    auto tensor_output = outputs_tu->elements()[3].toTensor().to(at::kCPU);//use the biggest disp 
    
-    tensor_output = tensor_output.permute({0,3,2,1});
+    tensor_output = tensor_output.permute({0,3,2,1});//change the dimentions of tensor
 
     cv::Mat disp=cv::Mat(h,w,CV_32FC1,tensor_output.data_ptr());
     cv::resize(disp,disp,cv::Size(src.cols,src.rows));

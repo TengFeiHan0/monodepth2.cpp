ca5f3b8e02617d3df00f7fdb153f6c2e5b1c3fe8
Tengfei han <38068286+TengFeiHan0@users.noreply.github.com>
2020-04-18 18:44:50 +0900

    add script for creating torchscript model

--- to_jit.py
+++ to_jit.py
@@ -1,17 +1,7 @@
-# Copyright 2020 Toyota Research Institute.  All rights reserved.
 
 """
-Evaluation script for depth estimation from monocular images
-Takes a pretrained pytorch model and calculates error metrics
-==========================================================
-Usage:
-make docker-evaluate-depth \
-MODEL=path/to/model.pth.tar (pretrained depth network) \
-INPUT_PATH=/path/to/split (for now only a KITTI split file or an image folder are supported) \
-DEPTH_TYPE=depth type used for evaluation [velodyne] \
-CROP=crop used in depth evaluation [garg] \
-SAVE_OUTPUT=/path/to/output (path where the output images and predicted depths will be saved)
-==========================================================
+this script is used for converting your pytorch model into torchscript that could be used under CPP environment. 
+The following code is extended from  ``` scripts/evaluate_depth.py ``` of  [packnet-sfm](https://github.com/TRI-ML/packnet-sfm),
 """
 import argparse
 import torch
